d348a435f13861ab21c8e2fedc0029d0d5a6f99f
leo.me <leo.me@kakaoenterprise.com>
2020-02-09 23:04:16 +0900

    CHAPTER 2-16

--- src/main/java/springbook/user/dao/UserDaoTest.java
+++ src/main/java/springbook/user/dao/UserDaoTest.java
@@ -16,11 +16,17 @@ import static org.junit.Assert.assertThat;
 
 public class UserDaoTest {
     private UserDao dao;
+    private User user1;
+    private User user2;
+    private User user3;
 
     @Before
     public void setUp() {
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         this.dao = context.getBean("userDao", UserDao.class);
+        this.user1 = new User("gyumee", "박성철", "springno1");
+        this.user2 = new User("leegw700", "이길원", "springno2");
+        this.user3 = new User("bumjin", "박범진", "springno3");
     }
 
     public static void main(String[] args) {
@@ -51,9 +57,6 @@ public class UserDaoTest {
 
     @Test
     public void count() throws SQLException {
-        User user1 = new User("gyumee", "박성철", "springno1");
-        User user2 = new User("leegw700", "이길원", "springno2");
-        User user3 = new User("bumjin", "박범진", "springno3");
 
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
