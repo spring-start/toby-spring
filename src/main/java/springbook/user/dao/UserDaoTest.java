33cdbfd07192e2f210b4741ab3475d0f31b4e11d
leo.me <leo.me@kakaoenterprise.com>
2020-02-09 23:28:52 +0900

    CHAPTER 2-19

--- src/main/java/springbook/user/dao/UserDaoTest.java
+++ src/main/java/springbook/user/dao/UserDaoTest.java
@@ -11,18 +11,24 @@ import org.springframework.context.ApplicationContext;
 
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import springbook.user.domain.User;
+
+import javax.sql.DataSource;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="/applicationContext.xml")
+@DirtiesContext
 public class UserDaoTest {
     @Autowired
     private ApplicationContext context;
+    @Autowired
     private UserDao dao;
     private User user1;
     private User user2;
@@ -32,7 +38,8 @@ public class UserDaoTest {
     public void setUp() {
         System.out.println(this.context);
         System.out.println(this);
-        this.dao = this.context.getBean("userDao", UserDao.class);
+        DataSource dataSource = new SingleConnectionDataSource("jdbc:mysql://localhost/testdb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC ", "spring", "book", true);
+        dao.setDataSource(dataSource);
         this.user1 = new User("gyumee", "박성철", "springno1");
         this.user2 = new User("leegw700", "이길원", "springno2");
         this.user3 = new User("bumjin", "박범진", "springno3");
