e41145f8fa3bf6b71d992629f80db43f2e3e914e
leo.me <leo.me@kakaoenterprise.com>
2020-02-09 22:36:03 +0900

    CHAPTER 2-8

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -61,4 +61,20 @@ public class UserDao {
         c.close();
     }
 
+    public int getCount() throws SQLException {
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return count;
+    }
+
 }
