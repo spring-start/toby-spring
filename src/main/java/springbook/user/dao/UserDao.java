d2a1dc1e7799f696591f359fff2d7a9ed8e44c13
django.franco <django.franco@kakaoenterprise.com>
2020-02-18 00:56:25 +0900

    CHAPTER 3-17

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -16,8 +16,20 @@ public class UserDao {
         this.dataSource = dataSource;
     }
 
-    public void add(User user) throws SQLException {
-        StatementStrategy st = new AddStatement(user);
+    public void add(final User user) throws SQLException {
+        class AddStatement implements StatementStrategy {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("insert into users(id, name, password)" +
+                        " values(?, ?, ?)");
+                ps.setString(1, user.getId());
+                ps.setString(2, user.getId());
+                ps.setString(3, user.getPassword());
+
+                return ps;
+            }
+        }
+        StatementStrategy st = new AddStatement();
         jdbcContextWithStatementStrategy(st);
     }
 
@@ -46,8 +58,7 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        StatementStrategy st = new DeleteAllStatement();
-        jdbcContextWithStatementStrategy(st);
+        return;
     }
 
     private void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
