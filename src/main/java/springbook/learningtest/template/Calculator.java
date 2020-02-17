d3d3b5e39f45e2c23db1565072a5ae62cc0e0d43
django.franco <django.franco@kakaoenterprise.com>
2020-02-18 03:34:05 +0900

    CHAPTER 3-32

---
+++ src/main/java/springbook/learningtest/template/Calculator.java
@@ -0,0 +1,29 @@
+package springbook.learningtest.template;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Calculator {
+    public Integer calcSum(String filepath) throws IOException {
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+            Integer sum = 0;
+            String line = null;
+            while((line = br.readLine()) != null) {
+                sum += Integer.valueOf(line);
+            }
+
+            return  sum;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            if (br != null) {
+                try { br.close(); } catch (IOException e) {System.out.println(e.getMessage()); }
+            }
+        }
+    }
+}
