f1e4feecc70738a303d87469693fc978b7435c02
django.franco <django.franco@kakaoenterprise.com>
2020-02-18 02:21:57 +0900

    CHAPTER 3-25

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -14,11 +14,9 @@ public class UserDao {
     private JdbcContext jdbcContext;
 
     public void setDataSource(DataSource dataSource) {
+        this.jdbcContext = new JdbcContext();
+        this.jdbcContext.setDataSource(dataSource);
         this.dataSource = dataSource;
-    }
-
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
     }
 
     public void add(final User user) throws SQLException {
