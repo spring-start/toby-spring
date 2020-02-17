ee27c4f29b30a720718c0fbc4961940c5c26e840
django.franco <django.franco@kakaoenterprise.com>
2020-02-18 03:58:23 +0900

    CHAPTER 3-40

--- src/main/java/springbook/learningtest/template/Calculator.java
+++ src/main/java/springbook/learningtest/template/Calculator.java
@@ -6,50 +6,44 @@ import java.io.IOException;
 
 public class Calculator {
     public Integer calcSum(String filepath) throws IOException {
-        BufferedReaderCallback sumCallback =
-                new BufferedReaderCallback() {
+        LineCallback sumCallback =
+                new LineCallback() {
                     @Override
-                    public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-                        Integer sum = 0;
-                        String line = null;
-                        while ((line = br.readLine()) != null) {
-                            sum += Integer.valueOf(line);
-                        }
-                        return sum;
+                    public Integer doSomethingWithLine(String line, Integer value) {
+                        return value + Integer.valueOf(line);
                     }
                 };
-        return fileReadTemplate(filepath, sumCallback);
+        return lineReadTemplate(filepath, sumCallback, 0);
     }
 
-    public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
+    public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException {
         BufferedReader br = null;
         try {
             br = new BufferedReader(new FileReader(filepath));
-            int ret = callback.doSomethingWithReader(br);
-            return ret;
-        } catch (IOException e) {
+            Integer res = initVal;
+            String line = null;
+            while((line = br.readLine()) != null) {
+                res = callback.doSomethingWithLine(line, res);
+            }
+            return  res;
+        } catch (IOException e){
             System.out.println(e.getMessage());
             throw e;
         } finally {
-            if (br != null) {
-                try { br.close(); } catch (IOException e) {System.out.println(e.getMessage()); }
+            if (br != null) try { br.close(); } catch (IOException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
 
     public Integer calcMultiply(String numFilepath) throws IOException {
-        BufferedReaderCallback multiplyCallback =
-                new BufferedReaderCallback() {
+        LineCallback multiplyCallback =
+                new LineCallback() {
                     @Override
-                    public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-                        Integer multiply = 1;
-                        String line = null;
-                        while((line = br.readLine()) != null) {
-                            multiply *= Integer.valueOf(line);
-                        }
-                        return multiply;
+                    public Integer doSomethingWithLine(String line, Integer value) {
+                        return value * Integer.valueOf(line);
                     }
                 };
-        return fileReadTemplate(numFilepath, multiplyCallback);
+        return lineReadTemplate(numFilepath, multiplyCallback, 1);
     }
 }
