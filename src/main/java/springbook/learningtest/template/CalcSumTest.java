d46fe72eee8f4661450ea48dcec41677fded8872
django.franco <django.franco@kakaoenterprise.com>
2020-02-18 03:48:37 +0900

    CHAPTER 3-32

--- src/main/java/springbook/learningtest/template/CalcSumTest.java
+++ src/main/java/springbook/learningtest/template/CalcSumTest.java
@@ -1,6 +1,7 @@
 package springbook.learningtest.template;
 
 import org.hamcrest.CoreMatchers;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -9,11 +10,21 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class CalcSumTest {
+    Calculator calculator;
+    String numFilepath;
+
+    @Before public void setUp() {
+        this.calculator = new Calculator();
+        this.numFilepath = getClass().getResource("/numbers.txt").getPath();
+    }
+
     @Test
     public void sumOfNumbers() throws IOException{
-        Calculator calculator = new Calculator();
-        int sum = calculator.calcSum(getClass().getResource(
-                "/numbers.txt").getPath());
-        assertThat(sum, is(10));
+        assertThat(calculator.calcSum(this.numFilepath), is(10));
+    }
+
+    @Test
+    public void multiplyOfNumbers() throws IOException {
+        assertThat(calculator.calcMultiply(this.numFilepath), is(24));
     }
 }
--- src/main/java/springbook/learningtest/template/Calculator.java
+++ src/main/java/springbook/learningtest/template/Calculator.java
@@ -6,17 +6,27 @@ import java.io.IOException;
 
 public class Calculator {
     public Integer calcSum(String filepath) throws IOException {
-        BufferedReader br = null;
+        BufferedReaderCallback sumCallback =
+                new BufferedReaderCallback() {
+                    @Override
+                    public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+                        Integer sum = 0;
+                        String line = null;
+                        while ((line = br.readLine()) != null) {
+                            sum += Integer.valueOf(line);
+                        }
+                        return sum;
+                    }
+                };
+        return fileReadTemplate(filepath, sumCallback);
+    }
 
+    public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
+        BufferedReader br = null;
         try {
             br = new BufferedReader(new FileReader(filepath));
-            Integer sum = 0;
-            String line = null;
-            while((line = br.readLine()) != null) {
-                sum += Integer.valueOf(line);
-            }
-
-            return  sum;
+            int ret = callback.doSomethingWithReader(br);
+            return ret;
         } catch (IOException e) {
             System.out.println(e.getMessage());
             throw e;
@@ -25,5 +35,21 @@ public class Calculator {
                 try { br.close(); } catch (IOException e) {System.out.println(e.getMessage()); }
             }
         }
+    }
+
+    public Integer calcMultiply(String numFilepath) throws IOException {
+        BufferedReaderCallback multiplyCallback =
+                new BufferedReaderCallback() {
+                    @Override
+                    public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+                        Integer multiply = 1;
+                        String line = null;
+                        while((line = br.readLine()) != null) {
+                            multiply *= Integer.valueOf(line);
+                        }
+                        return multiply;
+                    }
+                };
+        return fileReadTemplate(numFilepath, multiplyCallback);
     }
 }
