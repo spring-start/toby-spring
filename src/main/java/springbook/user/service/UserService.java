c6c032552b32f267159c61c2484a7f368d3cec78
leo.me <leo.me@kakaoenterprise.com>
2020-05-18 20:24:29 +0900

    Refactor UserService

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -16,25 +16,31 @@ public class UserService {
     public void upgradeLevels() {
         List<User> users = userDao.getAll();
         for (User user : users) {
-            Boolean changed = null;
-            if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
-                user.setLevel(Level.SILVER);
-                changed = true;
-            }
-            else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
-                user.setLevel(Level.GOLD);
-                changed = true;
-            }
-            else if (user.getLevel() == Level.GOLD) {
-                changed = false;
-            } else {
-                changed = false;
-            }
-
-            if (changed) {
-                userDao.update(user);
+            if (canUpgradeLevel(user)) {
+                upgradeLevel(user);
             }
         }
+    }
+
+    // Upgrade 조건을 확인하는 역할과 책임이 분리된다
+    private boolean canUpgradeLevel(User user) {
+        Level currentLevel = user.getLevel();
+        switch (currentLevel) {
+            case BASIC: return (user.getLogin() >= 50);
+            case SILVER: return (user.getRecommend() >= 30);
+            case GOLD: return false;
+            default: throw new IllegalArgumentException("Unknown Level: " + currentLevel);
+        }
+    }
+
+    // TODO : 나라면 level을 올리는 기능은 Level enum에서 직접 관리하도록 한다
+    private void upgradeLevel(User user) {
+        if (user.getLevel() == Level.BASIC) {
+            user.setLevel(Level.SILVER);
+        } else if (user.getLevel() == Level.SILVER) {
+            user.setLevel(Level.GOLD);
+        }
+        userDao.update(user);
     }
 
     public void add(User user) {
