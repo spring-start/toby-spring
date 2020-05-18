f373eab30b24fb7b5a7c81cb5802fbb5df60da93
leo.me <leo.me@kakaoenterprise.com>
2020-05-18 21:40:11 +0900

    Add Transcation to UserService

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -1,13 +1,23 @@
 package springbook.user.service;
 
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.interceptor.TransactionInterceptor;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
 import java.util.List;
 
 public class UserService {
     UserDao userDao;
+    private DataSource dataSource;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     public void setUserLevelUpgradePolicy(UserLevelUpgradePolicy userLevelUpgradePolicy) {
         this.userLevelUpgradePolicy = userLevelUpgradePolicy;
@@ -20,12 +30,28 @@ public class UserService {
     }
 
 
-    public void upgradeLevels() {
-        List<User> users = userDao.getAll();
-        for (User user : users) {
-            if (userLevelUpgradePolicy.canUpgradeLevel(user)) {
-                userLevelUpgradePolicy.upgradeLevel(user);
+    public void upgradeLevels() throws Exception{
+        // TODO : 코드 까보기
+        TransactionSynchronizationManager.initSynchronization();
+        // 이 메서드가 connection을 transaction 저장소에 저장해준다
+        Connection c = DataSourceUtils.getConnection(dataSource);
+        c.setAutoCommit(false);
+
+        try {
+            List<User> users = userDao.getAll();
+            for (User user : users) {
+                if (userLevelUpgradePolicy.canUpgradeLevel(user)) {
+                    userLevelUpgradePolicy.upgradeLevel(user);
+                }
             }
+            c.commit();
+        } catch (Exception e) {
+            c.rollback();
+            throw e;
+        } finally {
+            DataSourceUtils.releaseConnection(c, dataSource);
+            TransactionSynchronizationManager.unbindResource(this.dataSource);
+            TransactionSynchronizationManager.clearSynchronization();
         }
     }
 
