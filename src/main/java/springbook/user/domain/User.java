b9e5d7b2bc072433066efc9e9d68ad448e0d9029
leo.me <leo.me@kakaoenterprise.com>
2020-05-19 09:48:38 +0900

    Add javax.mail

--- src/main/java/springbook/user/domain/User.java
+++ src/main/java/springbook/user/domain/User.java
@@ -6,14 +6,16 @@ public class User {
     Level level;
     int login;
     int recommend;
+    String email;
 
-    public User(String id, String name, String password, Level level,  int login, int recommend) {
+    public User(String id, String name, String password, Level level,  int login, int recommend, String email) {
         this.id = id;
         this.name = name;
         this.password = password;
         this.level = level;
         this.login = login;
         this.recommend = recommend;
+        this.email = email;
     }
 
     public void upgradeLevel() {
@@ -72,5 +74,13 @@ public class User {
     }
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
--- src/main/java/springbook/user/service/BasicUserLevelUpgradePolicy.java
+++ src/main/java/springbook/user/service/BasicUserLevelUpgradePolicy.java
@@ -4,6 +4,14 @@ import springbook.user.dao.UserDao;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 public class BasicUserLevelUpgradePolicy implements UserLevelUpgradePolicy {
     public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
     public static final int MIN_RECOMMEND_FOR_GOLD = 30;
@@ -29,5 +37,25 @@ public class BasicUserLevelUpgradePolicy implements UserLevelUpgradePolicy {
     public void upgradeLevel(User user) {
         user.upgradeLevel();
         userDao.update(user);
+        sendUpgradeEMail(user);
+    }
+
+    private void sendUpgradeEMail(User user) {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "mail.ksug.org");
+        Session s = Session.getInstance(props, null);
+
+        MimeMessage message = new MimeMessage(s);
+        try {
+            message.setFrom(new InternetAddress("useradmin@ksug.org"));
+            message.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(user.getEmail()));
+            message.setSubject("Upgrade 안내");
+            message.setText("사용자님의 등급이 " + user.getLevel().name() + "로 업그레이트되었습니다");
+
+            Transport.send(message);
+        } catch ( MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
