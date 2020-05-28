6b79c6fa59e95ccf7b4a6f336a5caed7218c246d
django.franco <django.franco@kakaoenterprise.com>
2020-05-28 11:38:42 +0900

    ch-6-25 다이내믹프록시의 확장(1)

--- src/main/java/springbook/helloproxy/UppsercaseHandler.java
+++ src/main/java/springbook/helloproxy/UppsercaseHandler.java
@@ -4,15 +4,20 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public class UppsercaseHandler implements InvocationHandler {
-    Hello target;
+    Object target;
 
-    public UppsercaseHandler(Hello target) {
+    public UppsercaseHandler(Object target) {
         this.target = target;
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        String ret = (String)method.invoke(target, args);
-        return ret.toUpperCase();
+        Object ret = method.invoke(target, args);
+        if (ret instanceof String) {
+            return ((String)ret).toUpperCase();
+        }
+        else {
+            return ret;
+        }
     }
 }
