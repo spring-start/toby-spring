f38ae31be92c2be911a47823eec3e645a730b3b6
leo.me <leo.me@kakaoenterprise.com>
2020-06-10 22:45:54 +0900

    Resource를 이용한 추상화

--- src/main/java/springbook/user/sqlservice/JaxbXmlSqlReader.java
+++ src/main/java/springbook/user/sqlservice/JaxbXmlSqlReader.java
@@ -1,18 +1,23 @@
 package springbook.user.sqlservice;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import springbook.user.dao.UserDao;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class JaxbXmlSqlReader implements SqlReader {
     private static final String DEFAULT_SQLMAP_FILE = "sqlmap.xml";
-    private String sqlmapFile = DEFAULT_SQLMAP_FILE;
+    private Resource sqlmap = new ClassPathResource(DEFAULT_SQLMAP_FILE, UserDao.class);
 
-    public void setSqlmapFile(String sqlmapFile) {
-        this.sqlmapFile = sqlmapFile;
+    public void setSqlmap(Resource sqlmap) {
+        this.sqlmap = sqlmap;
     }
 
     @Override
@@ -21,14 +26,14 @@ public class JaxbXmlSqlReader implements SqlReader {
         try {
             JAXBContext context = JAXBContext.newInstance(contextPath);
             Unmarshaller unmarshaller = context.createUnmarshaller();
-            InputStream is = UserDao.class.getResourceAsStream(this.sqlmapFile);
-            Sqlmap sqlmap = (Sqlmap)unmarshaller.unmarshal(is);
+            Source source = new StreamSource(sqlmap.getInputStream());
+            Sqlmap sqlmap = (Sqlmap)unmarshaller.unmarshal(source);
 
             // 직접적으로 sqlmap에 접근하지 않고 sqlRegistery를 이용했음을 주목
             for(SqlType sql: sqlmap.getSql()) {
                 sqlRegistry.registerSql(sql.getKey(), sql.getValue());
             }
-        } catch (JAXBException e) {
+        } catch (JAXBException | IOException e) {
             throw new RuntimeException(e);
         }
 
